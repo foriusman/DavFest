34e767d3c07c250f79143fada7622c536d59a691
Usman Fori <45038694+foriusman@users.noreply.github.com>
2024-02-28 15:41:22 +0100

    Update RegisterBtn.jsx
    link to iwd 24

--- src/components/RegisterBtn.jsx
+++ src/components/RegisterBtn.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const RegisterBtn = () => (
-  <Link to='https://gdg.community.dev/events/details/google-gdg-maiduguri-presents-devfest-maiduguri-2023/' className="text-white-10 rounded-lg text-md font-light bg-green-30 py-2 px-5">Register</Link>
+  <Link to='https://gdg.community.dev/e/mcrhfy/' className="text-white-10 rounded-lg text-md font-light bg-blue-30 py-2 px-5">Register</Link>
 );
 
 export default RegisterBtn;
