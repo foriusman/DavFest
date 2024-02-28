b10f11d86588e159bd6f90754553e65e1fe266b7
Usman Fori <45038694+foriusman@users.noreply.github.com>
2024-02-28 15:40:22 +0100

    Update Recap.jsx
    recap wtm

--- src/components/Recap.jsx
+++ src/components/Recap.jsx
@@ -2,7 +2,7 @@ import back from '../assets/images/bg2.png';
 
 const Recap = () => (
   <section className='flex flex-col gap-6 p-6'>
-    <h2 className='text-center text-5xl font-semibold'>Recap DevFest 2022</h2>
+    <h2 className='text-center text-5xl font-semibold'>IWD Maiduguri 2023</h2>
     <div className='w-[70%] m-auto'>
       <img src={back} alt="Dev fest pictures" />
     </div>
