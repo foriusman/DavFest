9abc53732c80d6ef806d7ccde59254f705b4ec96
Usman Fori <45038694+foriusman@users.noreply.github.com>
2024-06-07 01:55:35 +0100

    Update About.jsx
    about io 24 updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,11 @@ const About = () => (
   <section className="bg-black text-white-10 px-[5%] pt-16 pb-10">
     <div className='flex flex-col-reverse lg:flex-row gap-20 max-w-7xl m-auto'>
       <div className="flex flex-col gap-8 lg:w-[75%] pt-16">
-        <h2 className="text-3xl font-semibold">International Women's Day Maiduguri 2024</h2>
-        <p>IWD is Women Techmakers' grand celebration, and this year, the theme is "Impact the Future", a call to action for women in tech to shape the future of technology and society for the better.</p>
-        <p>We believe in the power of our community to drive positive change, and we invite you to be a part of this impactful campaign</p>
-        <p>What to Expect: This year, get ready for a day filled with rockstar attendees and speakers, insightful sessions, networking opportunities, solution demos, hands-on projects, delicious food, awesome swags, and tons of fun!</p>
-        <p>Let's make International Women's Day Maiduguri 2024 a day to remember!</p>
+        <h2 className="text-3xl font-semibold"> Google IO Extended Maiduguri 2024</h2>
+        <p>Join us for an exciting day at Google IO Extended 2024, themed "Exploring AI,"</p>
+        <p>This event will delve into cutting-edge AI technologies and their applications. Attendees will explore topics such as "AI for Developers with Gemini in Android Studio," the latest features of "Android 15," critical insights on "Security & AI," and engage in a "Fireside Chat: IO Recap" with industry experts. This is a fantastic opportunity for developers, tech enthusiasts, and professionals to network, learn, and get inspired by the latest advancements in AI and Android. </p>
+        <p>Don't miss out on this free event hosted at the Abdul Samad Rabiu International Center for Innovation and Entrepreneurship, University of Maiduguri.</p>
+      
       </div>
 
       <div className='lg:w-[30%]'>
