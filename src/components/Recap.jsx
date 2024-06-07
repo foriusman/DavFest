04e7241d2fd347507b463765cf94746ec320e269
Usman Fori <45038694+foriusman@users.noreply.github.com>
2024-06-07 02:01:27 +0100

    Update Recap.jsx
    recap update

--- src/components/Recap.jsx
+++ src/components/Recap.jsx
@@ -2,9 +2,9 @@ import back from '../assets/images/bg2.png';
 
 const Recap = () => (
   <section className='flex flex-col gap-6 p-6'>
-    <h2 className='text-center text-5xl font-semibold'>IWD Maiduguri 2023</h2>
+    <h2 className='text-center text-5xl font-semibold'>Google IO Extended 2024</h2>
     <div className='w-[70%] m-auto'>
-      <img src={back} alt="Dev fest pictures" />
+      <img src={back} alt="ioextend pictures" />
     </div>
   </section>
 );
